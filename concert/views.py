10d21e71b9558fa2d0a73b48484829715b4c3c27
HossamJa <jamhosam123@gmail.com>
2025-08-16 16:37:54 -0400

    Connect Services in views.py and Finished the Dockerfile and Completed the Deployment File

--- concert/views.py
+++ concert/views.py
@@ -34,21 +34,14 @@ def index(request):
 
 
 def songs(request):
-    songs = {"songs":[{"id":1,"title":"duis faucibus accumsan odio curabitur convallis","lyrics":"Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis."}]}
+    # replace the {SONG_URL} with the songs microservice that you deployed
+    songs = req.get("{SONGS_URL}/song").json()
     return render(request, "songs.html", {"songs": songs["songs"]})
 
-
 def photos(request):
-    photos = [{
-    "id": 1,
-    "pic_url": "http://dummyimage.com/136x100.png/5fa2dd/ffffff",
-    "event_country": "United States",
-    "event_state": "District of Columbia",
-    "event_city": "Washington",
-    "event_date": "11/16/2022"
-}]
+    # replace the {PHOTO_URL} with the pictures microservice that you deployed
+    photos = req.get("{PHOTO_URL}/picture").json()
     return render(request, "photos.html", {"photos": photos})
-
 
 def login_view(request):
     if request.method == "POST":
